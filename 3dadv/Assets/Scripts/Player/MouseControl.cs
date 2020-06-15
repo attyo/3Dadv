981ee8a51ba024985f9a85bd2cf0b685e639e453
attyo <is0357kx@ed.ritsumei.ac.jp>
2020-06-15 17:51:21 +0900

    add プレイヤーのスクリプト
    とりあえずカメラとプレイヤーの制御スクリプト作った

--- 3dadv/Assets/Scripts/Player/MouseControl.cs
+++ 3dadv/Assets/Scripts/Player/MouseControl.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class MouseControl : MonoBehaviour
 {
+    [SerializeField]
+    GameObject camera;
+    [SerializeField]
+    float cam_speed = 2.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,19 @@ public class MouseControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        rotateCamera();
+    }
+    private void rotateCamera()
+    {
+        //Vector3でX,Y方向の回転の度合いを定義
+        Vector3 angle = new Vector3(Input.GetAxis("Mouse X") * cam_speed, Input.GetAxis("Mouse Y") * cam_speed, 0);
+
+
+        if (Mathf.Abs(camera.transform.rotation.eulerAngles.x) <= 60f)
+        {
+            
+        }
+        camera.transform.RotateAround(camera.transform.position, -camera.transform.right, angle.y);
+        camera.transform.RotateAround(camera.transform.position, Vector3.up, angle.x);
     }
 }
--- 3dadv/Assets/Scripts/Player/PlayerControl.cs
+++ 3dadv/Assets/Scripts/Player/PlayerControl.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
+    [SerializeField]
+    float speed=0.1f;
+    Rigidbody rigid;
+    Vector3 forward_camera;
+    Vector3 right_camera;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigid = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        forward_camera = transform.GetChild(0).transform.forward;//カメラのフォワードをとる
+        forward_camera.y = 0;
+        right_camera = transform.GetChild(0).transform.right;
+
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            rigid.MovePosition(transform.position + forward_camera * speed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rigid.MovePosition(transform.position - right_camera  * speed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rigid.MovePosition(transform.position - forward_camera * speed/2.0f);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rigid.MovePosition(transform.position + right_camera * speed);
+        }
     }
 }
