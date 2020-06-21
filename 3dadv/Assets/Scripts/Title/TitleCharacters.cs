4f210a78f81646c841d8384ef02f226ad217566f
KosukeOkajima <kuchito02@gmail.com>
2020-06-22 02:21:25 +0900

    Titleの作成
    背景とBGM以外は大体出来た．

---
+++ 3dadv/Assets/Scripts/Title/TitleCharacters.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using UniRx;
+
+public class TitleCharacters : MonoBehaviour
+{
+    private Text text;
+    private AudioSource audioSource;
+    private const string title = "The Advent";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<Text>();
+        audioSource = GetComponent<AudioSource>();
+
+        Observable.Interval(TimeSpan.FromMilliseconds(200))
+            .Take(title.Length)
+            .Subscribe(count =>
+            {
+                audioSource.Play();
+                audioSource.panStereo = UnityEngine.Random.Range(-1.0f, 1.0f);
+                text.text = title.Substring(0, (int)count + 1);
+            }).AddTo(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ 3dadv/Assets/Scripts/Title/TitleManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject fadeUI;
+    [SerializeField]
+    private GameObject startUI;
+
+    void Start()
+    {
+        StartCoroutine(WaitPressAnyKey());
+    }
+
+    IEnumerator WaitPressAnyKey()
+    {
+        yield return new WaitForSeconds(2.5f);
+
+        startUI.SetActive(true);
+
+        yield return new WaitUntil(() => Input.anyKeyDown);
+
+        fadeUI.SetActive(true);
+        
+        // 鳴らせたら効果音鳴らす
+        
+        // フェードと効果音の時間だけ待つ
+        yield return new WaitForSeconds(2);
+
+        SceneManager.LoadScene("dungon");
+    }
+}
